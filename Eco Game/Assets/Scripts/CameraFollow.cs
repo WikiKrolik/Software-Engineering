32569ce60d567a59ced090c9f4b57aa4c2dd7e4e
ArtJSON <artur.pietrzak1@outlook.com>
2022-12-07 18:15:30 +0100

    Add controller and camera follow script

---
+++ Eco Game/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+	public Transform target;
+	public float smoothTime = 0.3f;
+	private Vector3 velocity = Vector3.zero;
+
+	private void Update()
+	{
+		Vector3 targetPosition = target.TransformPoint(new Vector3(0, 0, -10));
+		transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+
+	}
+}
