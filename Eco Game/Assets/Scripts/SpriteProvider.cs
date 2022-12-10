ed8fc4d7fd4720594c89a718c07e4f14ecccb1cf
Kuba Pawlak <kuba_pawlak@icloud.com>
2022-12-10 21:41:03 +0100

    Created new branch and basic sprite provider script

---
+++ Eco Game/Assets/Scripts/SpriteProvider.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class SpriteProvider : MonoBehaviour
+    {
+
+        private SpriteRenderer spriteRenderer;
+
+        // Use this for initialization
+        void Start()
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (gameObject.name == "PlayerController")
+            {
+                spriteRenderer.sprite = null;
+            }
+        }
+    }
+}
