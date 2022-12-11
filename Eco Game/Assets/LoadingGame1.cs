a8406da74ce9b2e557fb48e297494c7cfa244bed
Magdalena Pakuła <magdalena.pakula001@gmail.com>
2022-12-12 00:20:43 +0100

    - added functions for 2 objects, - waiting for scenes to be added

---
+++ Eco Game/Assets/LoadingGame1.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingGame1 : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {  
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
