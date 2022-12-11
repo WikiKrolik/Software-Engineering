fa9f8af18b89b69c5066ecf9fed1f9df2efd6bd0
Jakub Wandelt <234772@edu.p.lodz.pl>
2022-12-11 14:42:01 +0100

    Added connecting to server

--- Eco Game/Assets/ServerConnector.cs
+++ Eco Game/Assets/ServerConnector.cs
@@ -2,11 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using UnityEngine.SceneManagement;
 
-public class ServerConnector : MonoBehaviour
+public class ServerConnector : MonoBehaviourPunCallbacks
 {
     private void Start()
     {
         PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        SceneManager.LoadScene("LobbyPlaceholder");
     }
 }
