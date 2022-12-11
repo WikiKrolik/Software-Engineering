5e086d46e9fa122eae29ad2eecd3269e49e7da76
Magdalena Pakuła <magdalena.pakula001@gmail.com>
2022-12-11 17:46:42 +0100

    - added classes from uml - enlarged mainGameplay to fit like other scenes

---
+++ Eco Game/Assets/Scripts/Analyzer.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Analyzer : MonoBehaviour
+{
+    private GameObject gameObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject = getAnimation<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Eco Game/Assets/Scripts/Animation.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation : MonoBehaviour
+{
+    private SpriteProvider spriteProvider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteProvider = getNextFrame<SpriteProvider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
