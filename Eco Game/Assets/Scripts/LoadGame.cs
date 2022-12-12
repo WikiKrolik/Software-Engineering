330a8f87c8fd5e5b61a94a713f4b550f30c3cb8f
Magdalena Pakuła <magdalena.pakula001@gmail.com>
2022-12-12 01:03:09 +0100

    - minigame works

---
+++ Eco Game/Assets/Scripts/LoadGame.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts
+{
+    public class LoadGame : MonoBehaviour
+    {
+        public string sceneName;
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.M))
+            {
+                SceneManager.LoadScene(sceneName,LoadSceneMode.Additive);
+            }
+        }
+    }
+}
